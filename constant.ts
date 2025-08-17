8d48f76a348bcd43ab1551d21de57dce207e7126
Jiovani <164600214+itsmeBani@users.noreply.github.com>
2025-08-17 12:25:16 +0800

    Update constant.ts

--- constant.ts
+++ constant.ts
@@ -1,15 +1,14 @@
 
 
-export const BACKEND_AUTH_BASE_URL = "https://musicappapi-aucja8e5ebdyfrbr.germanywestcentral-01.azurewebsites.net";
-
-export const SPOTIFY_CLIENT_ID = "70dfbbfacbc940c18b0ed206c231efdb";
-export const SPOTIFY_CLIENT_SECRET = "595ac8b9fb9d4fddade20acc7c4687d5";
+export const BACKEND_AUTH_BASE_URL = "
+export const SPOTIFY_CLIENT_ID = "";
+export const SPOTIFY_CLIENT_SECRET = "";
 
 export const SPOTIFY_DISCOVERY = {
-    issuer: "https://accounts.spotify.com",
-    authorizationEndpoint: "https://accounts.spotify.com/authorize",
-    tokenEndpoint: "https://accounts.spotify.com/api/token",
+    issuer: "",
+    authorizationEndpoint: "",
+    tokenEndpoint: "",
 };
 
-export const REDIRECT_URI = "com.jiovani.musicplayer://callback";
+export const REDIRECT_URI = "";
 
